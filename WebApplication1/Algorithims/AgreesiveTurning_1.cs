bee24d5f5d79ed68b5dbd595abf576779b27c41f
divasmalik <divasmalik22@gmail.com>
2017-05-22 01:51:40 +1000

    Enum Added and Agressive Turning Algo Added

--- WebApplication1/Algorithims/AgreesiveTurning_1.cs
+++ WebApplication1/Algorithims/AgreesiveTurning_1.cs
@@ -5,9 +5,65 @@ using System.Web;
 using WebApplication1.Models.DB;
 
 namespace WebApplication1.Algorithims
-{
-    public class AgreesiveTurning_1
+{   
+    public class AgreesiveTurning
     {
+        public void checkAgressiveTurning_And_LaneChange(IList<User_Driving_Data> Datalist, int frequency)
+        {
+            var listSize = Datalist.Count();
+            var RemainderOfList = listSize % frequency;
+            var DatachuckSize = listSize - RemainderOfList;
+            IList<User_Driving_Data> DataChuckList = new List<User_Driving_Data>();
+            double AgressiveTurningEventCount=0;
+            double LaneChangeEventCount = 0;
+            //LaneChange lanechange = new LaneChange();
+            for (int i=0; i<DatachuckSize; i=i+frequency)
+            {
+                for(int j=i; j<i+frequency; j++)
+                {
+                    DataChuckList.Add(Datalist.ElementAt(j));
+                }
+                var FirstEntry = DataChuckList.First();
+                var LastEntry = DataChuckList.Last();
+                var Heading = TurningAngle(FirstEntry, LastEntry);
+                if(Heading >= 30)
+                {
+                    //Create aggresive turning event
+                    AgressiveTurningEventCount++;
+                }
+                else 
+                if (Heading > 0 & Heading < 20 )
+                {
+                    // lanechange.CheckLaneChange(DataChuckList);
+                    LaneChangeEventCount++;
+                }
+                DataChuckList.Clear();
+            }
+            if (RemainderOfList != 0)
+            {
+                for (var i = DatachuckSize * frequency; i < listSize; i++)
+                {
+                    DataChuckList.Add(Datalist.ElementAt(i));
+                }
+                var FirstEntry = DataChuckList.First();
+                var LastEntry = DataChuckList.Last();
+                var Heading = TurningAngle(FirstEntry, LastEntry);
+                if (Heading >= 30)
+                {
+                    //Create aggresive turning event
+
+                }
+                else
+                if (Heading > 0 & Heading < 20)
+                {
+                    // lanechange.CheckLaneChange(DataChuckList);
+
+                }
+                DataChuckList.Clear();
+            }
+        }
+
+
         public double TurningAngle(User_Driving_Data Driving_Data1, User_Driving_Data Driving_Data2)
         {   
             double long1=Driving_Data1.Longitude;
@@ -23,28 +79,6 @@ namespace WebApplication1.Algorithims
             return brng;
         }
 
-        public bool Agreesive_Left_Turining(User_Driving_Data Driving_Data)
-        {
-            if(Driving_Data.User_Acceleration_X > 0.2)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        public bool Agreesive_Right_Turining(User_Driving_Data Driving_Data)
-        {
-            if (Driving_Data.User_Acceleration_X < -0.2)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
         public static double RadianToDegree(double radian)
         {
             return (180 / Math.PI) * radian;
---
+++ WebApplication1/Enum/EventType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Enum
+{
+    public enum EventType
+    {
+       Speeding_Event = 1,
+       Sudden_Acceleration =2,
+       Sudden_Braking = 3,
+       Agressive_Lane_Change=4,
+       Agressive_Turning = 5
+    }
+}
