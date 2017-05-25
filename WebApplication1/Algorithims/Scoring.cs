abcfed710a504a690933831500cdf7064bb7fa3a
yamin05 <yams.stj@gmail.com>
2017-05-25 22:07:38 +1000

    asd

--- WebApplication1/Algorithims/Scoring.cs
+++ WebApplication1/Algorithims/Scoring.cs
@@ -4,52 +4,70 @@ using System.Linq;
 using System.Web;
 using WebApplication1.Models.DB;
 using WebApplication1.Enums;
+using WebApplication1.Models.JsonModel;
 
 namespace WebApplication1.Algorithims
 {
     public class Scoring
     {
-        public string Score_Driving_Of_User(IList<User_Driving_Events> eventList)
+        private int LaneChangeCount = 0;
+        private int AggressiveTurningCount = 0;
+        private int SpeedingAndAccelerationCount = 0;
+        private int SuddenBrakingCount = 0;
+        private int SuddenAccelerationCount = 0;
+        private int SpeedingCount = 0;
+        private string score = null;
+
+        public ScoringModel Score_Driving_Of_User(IList<User_Driving_Events> eventList)
         {
-            var LaneChange = 0;
-            var AggressiveTurning = 0;
-            var SpeedingAndAccelerationEvents = 0;
-       
             foreach (var evnt in eventList)
             {
                 if (evnt.Event_Type_Id == (int)(EventType.Agressive_Lane_Change))
                 {
-                    LaneChange++;
+                    LaneChangeCount++;
                 }
-                else
-                if (evnt.Event_Type_Id == (int)(EventType.Agressive_Turning))
+                else if (evnt.Event_Type_Id == (int)(EventType.Agressive_Turning))
                 {
-                    AggressiveTurning++;
+                    AggressiveTurningCount++;
                 }
-                else
-                if (evnt.Event_Type_Id == (int)(EventType.Sudden_Acceleration) || 
-                    evnt.Event_Type_Id == (int)(EventType.Sudden_Braking) || 
-                    evnt.Event_Type_Id == (int)(EventType.Speeding_Event))
+                else if (evnt.Event_Type_Id == (int)(EventType.Sudden_Braking))
                 {
-                    SpeedingAndAccelerationEvents++;
+                    SuddenBrakingCount++;
+                }
+                else if (evnt.Event_Type_Id == (int)(EventType.Sudden_Acceleration))
+                {
+                    SuddenAccelerationCount++;
+                }
+                else if (evnt.Event_Type_Id == (int)(EventType.Speeding_Event))
+                {
+                    SpeedingCount++;
                 }
             }
-            if(LaneChange > 0)
+            SpeedingAndAccelerationCount = SuddenBrakingCount + SuddenAccelerationCount + SpeedingCount;
+            if (LaneChangeCount > 0)
             {
-                return "High Risk";
+                score = "High Risk";
             }
-            else if(AggressiveTurning > 0)
+            else if (AggressiveTurningCount > 0)
             {
-                return "Medium Risk";
+                score = "Medium Risk";
             }
-            else if(SpeedingAndAccelerationEvents>0)
+            else if (SpeedingAndAccelerationCount > 0)
             {
-                return "Moderate Risk";
+                score = "Moderate Risk";
             }
             else
             {
-                return "No Risk";
+                score = "No Risk";
             }
+            var scoringModel = new ScoringModel();
+            scoringModel.Agressive_Lane_Change_Count = LaneChangeCount;
+            scoringModel.Agressive_Turning_Count = AggressiveTurningCount;
+            scoringModel.Sudden_Acceleration_Count = SuddenAccelerationCount;
+            scoringModel.Sudden_Braking_Count = SuddenBrakingCount;
+            scoringModel.Speeding_Event_Count = SpeedingCount;
+            scoringModel.Session_Score = score;
+            return scoringModel;
         }
     }
 }
