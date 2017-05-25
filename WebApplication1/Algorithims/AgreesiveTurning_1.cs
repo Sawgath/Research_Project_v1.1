79fcc14ae03eb8fab1b15cf2c4d365ff03489ae4
divasmalik <divasmalik22@gmail.com>
2017-05-25 21:52:54 +1000

    Agressive Algo Updated

--- WebApplication1/Algorithims/AgreesiveTurning_1.cs
+++ WebApplication1/Algorithims/AgreesiveTurning_1.cs
@@ -98,14 +98,23 @@ namespace WebApplication1.Algorithims
             double long1=Driving_Data1.Longitude;
             double long2 = Driving_Data2.Longitude;
             double dLon = (long2-long1);
-            double y = Math.Sin(dLon) * Math.Cos(Driving_Data2.Latitude);
-            double x = Math.Cos(Driving_Data1.Latitude) * Math.Sin(Driving_Data2.Latitude) - Math.Sin(Driving_Data1.Latitude)
-                       * Math.Cos(Driving_Data2.Latitude) * Math.Cos(dLon);
-            double brng = Math.Atan2(y, x);
-            brng = RadianToDegree(brng);
-            brng = (brng + 360) % 360;
-            brng = 360 - brng;
-            return brng;
+            if (dLon != 0)
+            {
+                double y = Math.Sin(dLon) * Math.Cos(Driving_Data2.Latitude);
+                double x = Math.Cos(Driving_Data1.Latitude) * Math.Sin(Driving_Data2.Latitude) - Math.Sin(Driving_Data1.Latitude)
+                           * Math.Cos(Driving_Data2.Latitude) * Math.Cos(dLon);
+                double brng = Math.Atan2(y, x);
+                brng = RadianToDegree(brng);
+                brng = (brng + 360) % 360;
+                brng = 360 - brng;
+                return brng;
+            }
+            else
+            {
+                int brng = 0;
+                return brng;
+            }
+            
         }
 
         private static double RadianToDegree(double radian)
